486ffe008db21a9e6c8faf4336362360e84651b9
frikinomad <59007034+frikinomad@users.noreply.github.com>
2021-05-28 16:57:55 +0530

    updated settings.py
    removes secret key from settings.py

--- wallpaperWebsite/wallpaperWebsite/settings.py
+++ wallpaperWebsite/wallpaperWebsite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!t(2gb1d^u4_2%%s(nwk(&*byrzf7ltnpsb^82(4e($$)c439f'
+#  refer notes for secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
